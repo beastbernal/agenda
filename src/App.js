0940e5961b793eaa54aff3c1d3fc56050c4770b7
CodeDanielMartinez <danielmartinezdm@hotmail.com>
2020-06-16 20:04:18 -0500

    Estructura Card y Cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Cards from "./Components/Cards";
+import Card from "./Components/Card";
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Cards></Cards>
+      <Card></Card>
     </div>
   );
 }
---
+++ src/Components/Cards.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Cards extends Component {
+
+    render() {
+
+        return (
+            <div>
+                Aca se tiene que hacer un .map para renderizar cards
+            </div>
+        );
+    }
+}
+
+export default Cards;
